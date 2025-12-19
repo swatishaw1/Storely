e5fc6daad4e2f22a93f98c90b857d6c461344a22
Swati <swatisha@gmail.com>
2025-12-19 21:31:53 +0530

    first commit

---
+++ StorelyBackend/src/main/java/com/example/StorelyBackend/StorelyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.StorelyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorelyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorelyBackendApplication.class, args);
+	}
+
+}
